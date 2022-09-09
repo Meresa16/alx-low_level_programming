7e14e2b2eaba3e2a0b90f7ff08b075499f275846
meresa16 <meresagidey0938@gmail.com>
2022-09-09 13:52:21 -0700

    shell variables expansion

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
+ * main -Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int z;
+	char c = 'a';
+	int num;
 
-for (z = 48; z <= 57; z++)
-{
-putchar(z);
-}
-for (z = 97; z <= 102;z++)
-{
-putchar(z)
-}
-putchar('\n');
-return (0);
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		} else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
 }
