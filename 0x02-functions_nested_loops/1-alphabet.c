022046c3de18f38d927e5c92e91fbfb39050b76c
meresa16 <meresagidey0938@gmail.com>
2022-09-13 10:30:45 -0700

    commit function nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,17 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_alphabet - check the code for Holberton School students.
+ * print_alphabet - Print lowercase alphabets
  *
- * Return: Always 0.
+ *
+ * Return: Always (0)
  */
-
 void print_alphabet(void)
 {
-	char alp = 'a';
-
-	while (alp <= 'z')
-	{
-		_putchar (alp);
-		alp++;
-
-	}
-	_putchar ('\n');
-
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
 }
