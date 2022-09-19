172739c10a90c756abf9c8e2fba3928e75c9ab8a
meresa16 <meresagidey0938@gmail.com>
2022-09-19 10:48:37 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * reset_to_98 - main
- * @n: - pinter to int
+ * reset_to_98 - changes the value of the variable
+ * that's pointed to by the argument to 98
+ *@n: pointer variable
  */
-
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
-
